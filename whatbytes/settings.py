6d2c221d6cad973a998444b2e4e35bb13b2aaf05
Sarthak Vishwakarma <vsarthak62@gmail.com>
2024-11-27 21:08:53 +0530

    fourth change

--- whatbytes/settings.py
+++ whatbytes/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -70,6 +71,9 @@ TEMPLATES = [
         },
     },
 ]
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
+
 
 WSGI_APPLICATION = 'whatbytes.wsgi.application'
 
